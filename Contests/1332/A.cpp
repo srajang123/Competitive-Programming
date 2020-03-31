c52e1d4c3af0d1fef38fdae8d6b3cbd3f952a368
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-03-31 23:40:54 +0530

    New Codes

---
+++ Contests/1332/A.cpp
@@ -0,0 +1,171 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000005
+#define M 1000000007
+
+//Prime Numbers
+
+vector<bool>prime(N+1,true);
+void sieve()
+{
+	ll i,j,k;
+	prime[0]=prime[1]=false;
+	for(i=2;i*i<=N;i++)
+	{
+		if(prime[i])
+		{
+			for(j=i*i;j<=N;j+=i)
+			{
+				prime[j]=false;
+			}
+		}
+	}
+}
+
+//Exponentiation
+
+ll power(ll a,ll b)
+{
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=r*a;
+		b/=2;
+		a*=a;		
+	}
+	return r;
+}
+ll power(ll a,ll b,ll m)
+{
+	a=a%m;
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=(r*a)%m;
+		b/=2;
+		a=(a*a)%m;		
+	}
+	return r;
+}
+
+//Prime Factors
+
+vector<ll> factors(ll n)
+{
+	vector<ll>r;
+	ll i,j;
+	for(i=1;i*i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			r.push_back(i);
+			if(n/i!=i)
+				r.push_back(n/i);
+		}
+	}
+	return r;
+}
+
+//GCD
+ll gcd(ll a,ll b)
+{
+	if(b>a)
+	{
+		ll t=a;
+		a=b;
+		b=t;
+	}
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+ll lcm(ll a,ll b)
+{
+	return (a*b)/gcd(a,b);
+}
+//Graphs
+vector<bool>bvisited(N,false);
+vector<ll> bfs(ll V,vector<vector<ll>>G,ll s)
+{
+	vector<ll>order;
+	queue<ll>q;
+	bvisited[s]=true;
+	q.push(s);
+	while(!q.empty())
+	{
+		s=q.front();
+		order.push_back(s);
+		for(auto x:G[s])
+		{
+			if(!bvisited[x])
+			{
+				bvisited[x]=true;
+				q.push(x);
+			}
+		}
+	}
+	return order;
+}
+vector<bool>dvisited(N,false);
+vector<ll> dfs(ll V,vector<vector<ll>>G,ll s)
+{
+	vector<ll>order;
+	stack<ll>q;
+	q.push(s);
+	while(!q.empty())
+	{
+		ll v=q.top();
+		q.pop();
+		if(!dvisited[v])
+		{
+			order.push_back(v);
+			dvisited[v]=true;
+		}
+		for(ll i=0;i<G[v].size();i++)
+		{
+			if(!dvisited[G[v][i]])
+			{
+				q.push(G[v][i]);
+			}
+		}
+	}
+	return order;
+}
+
+//Main Solution
+
+void solve()
+{
+	ll a,b,c,d,x,y,p,q,r,s;
+	cin>>a>>b>>c>>d;
+	cin>>x>>y>>p>>q>>r>>s;
+	bool f=true;
+	if((a>0 && x<=p) &&(b>0 && x>=r))f=false;
+	if((c>0 && y<=q) && (d>0 && y>=s))f=false;
+	a=b-a;
+	if(a<0 && (x+a)<p)f=false;
+	if(a>0 && (x+a)>r)f=false;
+	c=d-c;
+	if(c<0 && (y+c)<q)f=false;
+	if(c>0 && (y+c)>s)f=false;
+	if(f)
+		cout<<"Yes";
+	else
+		cout<<"No";
+	cout<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.tie(NULL);
+    ll t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
