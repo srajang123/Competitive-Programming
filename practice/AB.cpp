df31e21b1e058035f0f8294f38c76a0743d1ba85
SRAJAN GUPTA <srajan@ubuntu.ubuntu-domain>
2020-04-24 19:46:29 +0530

    Updated with Multiple codes

---
+++ practice/AB.cpp
@@ -0,0 +1,184 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000005
+#define M 1000000007
+
+//Prime Numbers
+/*
+ouuuann
+ouuuam
+owuann
+owuam
+ouwann
+ouwam
+*/
+vector<bool>prime(N+1,true);
+void sieve()
+{
+	ll i,j,k;
+	prime[0]=prime[1]=false;
+	for(i=2;i*i<=N;i++)
+	{
+		if(prime[i])
+		{
+			for(j=i*i;j<=N;j+=i)
+			{
+				prime[j]=false;
+			}
+		}
+	}
+}
+
+//Exponentiation
+
+ll power(ll a,ll b)
+{
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=r*a;
+		b/=2;
+		a*=a;		
+	}
+	return r;
+}
+ll power(ll a,ll b,ll m)
+{
+	a=a%m;
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=(r*a)%m;
+		b/=2;
+		a=(a*a)%m;		
+	}
+	return r;
+}
+
+//Prime Factors
+
+vector<ll> factors(ll n)
+{
+	vector<ll>r;
+	ll i,j;
+	for(i=1;i*i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			r.push_back(i);
+			if(n/i!=i)
+				r.push_back(n/i);
+		}
+	}
+	return r;
+}
+
+//GCD
+ll gcd(ll a,ll b)
+{
+	if(b>a)
+	{
+		ll t=a;
+		a=b;
+		b=t;
+	}
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+ll lcm(ll a,ll b)
+{
+	return (a*b)/gcd(a,b);
+}
+//Graphs
+vector<bool>bvisited(N,false);
+vector<ll> bfs(ll V,vector<vector<ll>>G,ll s)
+{
+	vector<ll>order;
+	queue<ll>q;
+	bvisited[s]=true;
+	q.push(s);
+	while(!q.empty())
+	{
+		s=q.front();
+		order.push_back(s);
+		for(auto x:G[s])
+		{
+			if(!bvisited[x])
+			{
+				bvisited[x]=true;
+				q.push(x);
+			}
+		}
+	}
+	return order;
+}
+vector<bool>dvisited(N,false);
+vector<ll> dfs(ll V,vector<vector<ll>>G,ll s)
+{
+	vector<ll>order;
+	stack<ll>q;
+	q.push(s);
+	while(!q.empty())
+	{
+		ll v=q.top();
+		q.pop();
+		if(!dvisited[v])
+		{
+			order.push_back(v);
+			dvisited[v]=true;
+		}
+		for(ll i=0;i<G[v].size();i++)
+		{
+			if(!dvisited[G[v][i]])
+			{
+				q.push(G[v][i]);
+			}
+		}
+	}
+	return order;
+}
+
+//Main Solution
+
+void solve()
+{
+	string s;
+	ll i,j,k,n;
+	cin>>s;
+	n=s.size();
+	vector<ll>a(n+1,0);
+	a[0]=a[1]=1;
+	bool f=true;
+	i=0;
+		if(s[i]=='w'||s[i]=='m')f=false;
+	for(i=1;i<n && f;i++)
+	{	
+		if(s[i]=='w'||s[i]=='m')f=false;
+		else
+		{
+			a[i+1]=a[i]%M;
+			if((s[i]=='u' && s[i-1]=='u')||(s[i]=='n' && s[i-1]==s[i]))
+				a[i+1]=(a[i+1]+a[i-1])%M;;
+		}
+	}
+	
+	if(f)
+		cout<<a[n]%M;
+	else
+		cout<<0;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.tie(NULL);
+    ll t=1;
+    while(t--)
+    {
+        solve();
+    }
+}
