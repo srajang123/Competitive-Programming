2be97d6405f3120fb805ee4b6624a880a54e3f5a
Srajan Gupta <srajan.oel@gmail.com>
2020-10-13 22:56:58 +0530

    Added Sorting in DSA

--- DSA/Sorting/Merge-Sort.cpp
+++ DSA/Sorting/Merge-Sort.cpp
@@ -7,6 +7,36 @@ using namespace std;
     Stable Sort: Yes
 */
 
+void Merge(int a[],int l,int md,int r)
+{
+    int i=l,j=md+1,k=0;
+    int b[r-l+1];
+    while(i<=md && j<=r)
+    {
+        if(a[i]<=a[j])
+            b[k++]=a[i++];
+        else
+            b[k++]=a[j++];
+    }
+    while(i<=md)    
+        b[k++]=a[i++];
+    while(j<=r)    
+        b[k++]=a[j++];
+    for(i=l;i<=r;i++)
+        a[i]=b[i-l];
+}
+
+void MergeSort(int a[],int l,int r)
+{
+    if(l<r)
+    {
+        int md=(l+r)/2;
+        MergeSort(a,l,md);
+        MergeSort(a,md+1,r);
+        Merge(a,l,md,r);
+    }
+}
+
 int main()
 {
     int a[100],i,j,k,n;
@@ -14,18 +44,7 @@ int main()
     cin>>n;
     for(i=0;i<n;i++)
         cin>>a[i];
-    for(i=0;i<n-1;i++)
-    {
-        k=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(a[j]<a[k])
-                k=j;
-        }
-        j=a[k];
-        a[k]=a[i];
-        a[i]=j;
-    }
+    MergeSort(a,0,n-1);
     cout<<"The sorted Array is:\n";
     for(i=0;i<n;i++)
         cout<<a[i]<<" ";
