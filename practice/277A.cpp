9a5d2c2efb5cd49398ec257d7ac0cc684070451e
Srajan Gupta <srajan.oel@gmail.com>
2020-10-21 22:32:37 +0530

    Added practice problems

---
+++ practice/277A.cpp
@@ -0,0 +1,219 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000005
+#define M 1000000007
+
+/*
+	*********************************************************************
+	*                             Code By                               *
+	*                                                                   *
+	*                            Srajan Gupta                           *
+	*                            srajang_123                            *
+	*                                                                   *
+	*********************************************************************
+*/
+//Prime Numbers
+
+vector<bool>prime(N+1,true);
+void sieve()
+{
+	ll i,j,k;
+	prime[0]=prime[1]=false;
+	for(i=2;i*i<=N;i++)
+	{
+		if(prime[i])
+		{
+			for(j=i*i;j<=N;j+=i)
+			{
+				prime[j]=false;
+			}
+		}
+	}
+}
+
+//Exponentiation
+
+ll power(ll a,ll b)
+{
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=r*a;
+		b/=2;
+		a*=a;		
+	}
+	return r;
+}
+ll power(ll a,ll b,ll m)
+{
+	a=a%m;
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=(r*a)%m;
+		b/=2;
+		a=(a*a)%m;		
+	}
+	return r;
+}
+
+//Prime Factors
+
+vector<ll> factors(ll n)
+{
+	vector<ll>r;
+	ll i,j;
+	for(i=1;i*i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			r.push_back(i);
+			if(n/i!=i)
+				r.push_back(n/i);
+		}
+	}
+	return r;
+}
+
+//GCD
+ll gcd(ll a,ll b)
+{
+	if(b>a)
+	{
+		ll t=a;
+		a=b;
+		b=t;
+	}
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+ll lcm(ll a,ll b)
+{
+	return (a*b)/gcd(a,b);
+}
+//Graphs
+vector<bool>bvisited(N,false);
+vector<vector<ll>>G(N);
+vector<ll> bfs(ll s)
+{
+	vector<ll>order;
+	queue<ll>q;
+	bvisited[s]=true;
+	q.push(s);
+	while(!q.empty())
+	{
+		s=q.front();
+		q.pop();
+		order.push_back(s);
+		for(auto x:G[s])
+		{
+			if(!bvisited[x])
+			{
+				bvisited[x]=true;
+				q.push(x);
+			}
+		}
+	}
+	return order;
+}
+vector<bool>dvisited(N,false);
+vector<ll> dfs(ll s)
+{
+	vector<ll>order;
+	stack<ll>q;
+	q.push(s);
+	while(!q.empty())
+	{
+		ll v=q.top();
+		q.pop();
+		if(!dvisited[v])
+		{
+			order.push_back(v);
+			dvisited[v]=true;
+		}
+		for(ll i=0;i<G[v].size();i++)
+		{
+			if(!dvisited[G[v][i]])
+			{
+				q.push(G[v][i]);
+			}
+		}
+	}
+	return order;
+}
+//My Functions
+
+void print(pair<ll,ll>a)
+{
+	cout<<a.first<<" "<<a.second;
+}
+void print(vector<ll>a)
+{
+	for(auto x:a)
+		cout<<x<<" ";
+}
+bool sortbysec(const pair<ll,ll>&a,const pair<ll,ll>&b)
+{
+	return a.second<b.second;
+}
+//Main Solution
+
+void solve()
+{
+	ll n,m,i,j,k,l=0,u=0,v;
+	cin>>n>>m;
+	vector<vector<ll>>g(m+1);
+	vector<ll>d(m+1,0);
+	for(i=1;i<=n;i++)
+	{
+		cin>>j;
+		if(j==0)u++;
+		vector<ll>a(j);
+		for(j=0;j<a.size();j++)
+		{
+			cin>>a[j];
+			d[a[j]]++;
+		}
+		for(j=1;j<a.size();j++)
+		{
+			G[a[0]].push_back(a[j]);
+			G[a[j]].push_back(a[0]);
+		}
+	}
+	/*for(i=1;i<=m;i++)
+	{
+	    cout<<i<<":";
+	    for(auto x:G[i])
+	        cout<<x<<" ";
+	    cout<<"\n";
+	}*/
+	for(i=1;i<=m;i++)
+	{
+		if(!dvisited[i] && d[i]>0)
+		{
+		    //cout<<i<<":\n";
+			dfs(i);
+			l++;
+		}
+	}
+	l--;
+	if(l<0)l=0;
+	l+=u;
+	cout<<l<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.tie(NULL);
+    ll t=1;
+	//cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
