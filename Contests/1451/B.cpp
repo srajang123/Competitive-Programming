4e28392a06b73643f92d23d02f865038daa5acf0
Srajan Gupta <srajan.oel@gmail.com>
2020-11-22 02:13:22 +0530

    Added Codeforces round #685(Div. 2)

---
+++ Contests/1451/B.cpp
@@ -0,0 +1,227 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define N 1000005
+#define M 1000000007
+
+/*
+	*********************************************************************
+	*                             Code By                               *
+	*                                                                   *
+	*                            Srajan Gupta                           *
+	*                            srajang_123                            *
+	*                                                                   *
+	*********************************************************************
+*/
+//Prime Numbers
+
+vector<bool>prime(N+1,true);
+void sieve()
+{
+	ll i,j,k;
+	prime[0]=prime[1]=false;
+	for(i=2;i*i<=N;i++)
+	{
+		if(prime[i])
+		{
+			for(j=i*i;j<=N;j+=i)
+			{
+				prime[j]=false;
+			}
+		}
+	}
+}
+
+//Exponentiation
+
+ll power(ll a,ll b)
+{
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=r*a;
+		b/=2;
+		a*=a;		
+	}
+	return r;
+}
+ll power(ll a,ll b,ll m)
+{
+	a=a%m;
+	ll r=1;
+	while(b)	
+	{
+		if(b%2==1)
+			r=(r*a)%m;
+		b/=2;
+		a=(a*a)%m;		
+	}
+	return r;
+}
+
+//Prime Factors
+
+vector<ll> factors(ll n)
+{
+	vector<ll>r;
+	ll i,j;
+	for(i=1;i*i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			r.push_back(i);
+			if(n/i!=i)
+				r.push_back(n/i);
+		}
+	}
+	return r;
+}
+
+//GCD
+ll gcd(ll a,ll b)
+{
+	if(b>a)
+	{
+		ll t=a;
+		a=b;
+		b=t;
+	}
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+ll lcm(ll a,ll b)
+{
+	return (a*b)/gcd(a,b);
+}
+//Graphs
+vector<bool>bvisited(N,false);
+vector<vector<ll>>G(N);
+vector<ll> bfs(ll s)
+{
+	vector<ll>order;
+	queue<ll>q;
+	bvisited[s]=true;
+	q.push(s);
+	while(!q.empty())
+	{
+		s=q.front();
+		q.pop();
+		order.push_back(s);
+		for(auto x:G[s])
+		{
+			if(!bvisited[x])
+			{
+				bvisited[x]=true;
+				q.push(x);
+			}
+		}
+	}
+	return order;
+}
+vector<bool>dvisited(N,false);
+vector<ll> dfs(ll s)
+{
+	vector<ll>order;
+	stack<ll>q;
+	q.push(s);
+	while(!q.empty())
+	{
+		ll v=q.top();
+		q.pop();
+		if(!dvisited[v])
+		{
+			order.push_back(v);
+			dvisited[v]=true;
+		}
+		for(ll i=0;i<G[v].size();i++)
+		{
+			if(!dvisited[G[v][i]])
+			{
+				q.push(G[v][i]);
+			}
+		}
+	}
+	return order;
+}
+//My Functions
+
+void print(pair<ll,ll>a)
+{
+	cout<<a.first<<" "<<a.second;
+}
+void print(vector<ll>a)
+{
+	for(auto x:a)
+		cout<<x<<" ";
+}
+bool sortbysec(const pair<ll,ll>&a,const pair<ll,ll>&b)
+{
+	return a.second<b.second;
+}
+//Main Solution
+bool primec(ll n)
+{
+    if(n<N)
+        return prime[n];
+    ll i;
+    for(i=2;i*i<=n;i++)
+    {
+        if(n%i==0)return false;
+    }
+    return true;
+}
+bool isSub(string a,string b,ll n,ll m,ll c=0,ll bb=0)
+{
+    if(m==0)
+    {
+        if(bb==1)
+            return true;
+        return false;
+    }
+    if(n==0)return false;
+    bool k=true;
+    if(a[n-1]==b[m-1])
+    {
+        k=isSub(a,b,n-1,m-1,c+1,bb);
+        if(bb==0)
+            k|=isSub(a,b,n-1,m,c+1,1);
+        return k;
+    }
+    if(c>0)
+        bb=1;
+    return isSub(a,b,n-1,m,c,bb);
+}
+void solve()
+{
+    ll i,j,k,n,m,l;
+    string s;
+    cin>>n>>m>>s;
+    bool f=true;
+    while(m--)
+    {
+        cin>>i>>j;
+        string t=s.substr(i-1,j-i+1);
+        f=isSub(s,t,n,j-i+1);
+        if(f)   
+            cout<<"YES";
+        else
+            cout<<"NO";
+        cout<<"\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.tie(NULL);
+    ll t=1;
+	cin>>t;
+	sieve();
+    while(t--)
+    {
+        solve();
+    }
+}
+
